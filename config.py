3f55fc29f1fd27914584930974c3a0170b9a14a6
PatC86 <pgrcronin@hotmail.co.uk>
2025-07-28 21:26:48 +0100

    Dev update 3

--- config.py
+++ config.py
@@ -4,8 +4,11 @@
 # Updated   : 20/07/2025
 # Purpose   : Db config settings.
 
-# Secret key and database uri hardcoded for project would place in .env file in real world situation.
+from dotenv import load_dotenv
+import os
+
 class Config:
-    SECRET_KEY = 'IHAVEASTINKYCAT'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://fp_lchproddb_user:IY8HP1nd2RZEDHcRGdoOurUjEtIam7Eh@dpg-d1tqmhk9c44c73cbdjl0-a.frankfurt-postgres.render.com/fp_lchproddb'
+    load_dotenv()
+    secret_key = os.getenv('SECRET_KEY')
+    sqlalchemy_database_uri = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
