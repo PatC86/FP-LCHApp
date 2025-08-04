228251d02a6443b6cb6482a30780ef0013b0a019
PatC86 <pgrcronin@hotmail.co.uk>
2025-08-04 12:46:24 +0100

    Dev update 14 - Dummy test add

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,4 @@
-# Name      : main
+# Name      : __init__
 # Author    : Patrick Cronin
 # Date      : 20/07/2025
 # Updated   : 0408/2025
---
+++ website/test___init__.py
@@ -0,0 +1,10 @@
+# Name      : test__init__
+# Author    : Patrick Cronin
+# Date      : 20/07/2025
+# Updated   : 0408/2025
+# Purpose   : Test functions defined in __init__
+
+
+
+def test_create_app():
+    assert True
