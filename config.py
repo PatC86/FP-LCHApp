f0f47b4ac7a9280f9258be1c9762976d447c6441
PatC86 <pgrcronin@hotmail.co.uk>
2025-08-17 21:23:13 +0100

    Dev update 21 - Increased error handling

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Name      : config
 # Author    : Patrick Cronin
 # Date      : 19/07/2025
-# Updated   : 03/08/2025
+# Updated   : 17/08/2025
 # Purpose   : Config settings for application.
 
 from dotenv import load_dotenv
@@ -9,7 +9,13 @@ import os
 
 class Config:
     load_dotenv()
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
-    #SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    try:
+        SECRET_KEY = os.getenv('SECRET_KEY')
+    except KeyError:
+        raise EnvironmentError('Please set the SECRET_KEY env variable')
+    try:
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+        #SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    except Exception as e:
+        raise EnvironmentError('Please set the DATABASE_URI env variable')
     #SQLALCHEMY_TRACK_MODIFICATIONS = False
--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 # Name      : main
 # Author    : Patrick Cronin
 # Date      : 19/07/2025
-# Updated   : 19/07/2025
+# Updated   : 17/08/2025
 # Purpose   : Main Python file for running lifting chain application.
+import sys
 
 from website import create_app
+import logging
 
-app = create_app()
+try:
+    app = create_app()
+except Exception as e:
+    logging.error(f'Failed to create app: {e}')
+    sys.exit(1)
+
 
 ## run app would turn off debug if production env
 if __name__ == '__main__':
-    app.run(debug=True)
+    try:
+        app.run(debug=True)
+    except Exception as e:
+        logging.error(f'Failed to run app: {e}')
+        sys.exit(1)
